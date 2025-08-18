eaf628f5bcaa57d0e24218266d0c8774e2d29e09
ikenith <kamalkumarthakur07+git@gmail.com>
2025-08-19 00:29:28 +0530

    deleted fluxdo, will do later facing erorrs

--- fluxdo/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: '/fluxdo/', // ✅ correct for GitHub Pages (repo folder name)
-    plugins: [react()],
-})
